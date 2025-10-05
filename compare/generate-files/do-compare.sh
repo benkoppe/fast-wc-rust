39cb47efcafeaa90cd10ac9d38456695ab55045b
Ben <koppe.development@gmail.com>
2025-10-04 23:23:08 -0400

    Add bang header to all scripts

--- compare/generate-files/do-compare.sh
+++ compare/generate-files/do-compare.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 python3 generate-large-files.py
 
 ../run-all.sh ./generated_input/
--- competitors/fast-cpp/compile.sh
+++ competitors/fast-cpp/compile.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 c++ -O3 -std=c++20 fast-wc.cpp utils.cpp -lpthread -lstdc++fs -o fast-wc
