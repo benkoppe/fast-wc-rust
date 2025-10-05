2ac738f8cbf90cb1ba4728b23553c85bdd5fb429
Ben <koppe.development@gmail.com>
2025-10-04 21:17:25 -0400

    Update run-all to benchmark with hyperfine

--- compare/run-all.sh
+++ compare/run-all.sh
@@ -14,13 +14,14 @@ if [[ ! -d "$DIR" ]]; then
   exit 1
 fi
 
-echo "Compiling all..."
+echo "Building..."
 cargo build --release --manifest-path "$SCRIPT_DIR/../fast-wc-rust/Cargo.toml"
 "$SCRIPT_DIR/../competitors/fast-cpp/compile.sh"
 
-echo "Running all..."
-echo "RUST:"
-"$SCRIPT_DIR/../fast-wc-rust/target/release/fast-wc-rust" "$DIR"
-
-echo "C++:"
-"$SCRIPT_DIR/../competitors/fast-cpp/fast-wc" "$DIR"
+echo
+echo "Benchmarking with hyperfine..."
+hyperfine \
+  --warmup 2 \
+  --runs 10 \
+  "$SCRIPT_DIR/../fast-wc-rust/target/release/fast-wc-rust $DIR" \
+  "$SCRIPT_DIR/../competitors/fast-cpp/fast-wc $DIR"
