3a15bdbcba62d3186576430980b1c1e975158900
Ben <koppe.development@gmail.com>
2025-10-05 11:29:36 -0400

    Use absolute paths in compile.sh

--- competitors/fast-cpp/compile.sh
+++ competitors/fast-cpp/compile.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-c++ -O3 -std=c++20 fast-wc.cpp utils.cpp -lpthread -lstdc++fs -o fast-wc
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+c++ -O3 -std=c++20 \
+  "$SCRIPT_DIR/fast-wc.cpp" \
+  "$SCRIPT_DIR/utils.cpp" \
+  -lpthread -lstdc++fs \
+  -o "$SCRIPT_DIR/fast-wc"
